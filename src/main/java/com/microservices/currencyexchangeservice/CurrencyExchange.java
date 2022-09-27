693ced8b1a63f47894fbd50ffbb8da7ed9361e72
SylwiaWojda <sylwiaaa001@wp.pl>
2022-09-27 10:47:51 +1000

    Configure JPA and Initialized Data

--- src/main/java/com/microservices/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,19 +1,34 @@
 package com.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.lang.constant.Constable;
 import java.math.BigDecimal;
-
+@Entity
 public class CurrencyExchange {
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
+    private String environment;
 
     public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
         this.id = id;
         this.from = from;
         this.to = to;
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+        this.environment = environment;
     }
 
     public CurrencyExchange() {
@@ -49,5 +64,13 @@ public class CurrencyExchange {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
     }
 }
